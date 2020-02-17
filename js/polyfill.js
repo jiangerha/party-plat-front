f631b0f3de774cec099149e8f81dc3b3b2dbb99f
谢倩 <dora@xieqiandeMacBook-Pro.local>
2020-02-17 22:01:21 +0800

    fix bug

---
+++ js/polyfill.js
@@ -0,0 +1,13 @@
+/* Polyfill service v3.25.1
+ * For detailed credits and licence information see https://github.com/financial-times/polyfill-service.
+ * 
+ * UA detected: chrome/80.0.0
+ * Features requested: Element.prototype.classList,Element.prototype.closest,Element.prototype.matches,Node.prototype.contains
+ *  */
+
+(function(undefined) {
+
+    /* No polyfills found for current settings */
+    
+    })
+    .call('object' === typeof window && window || 'object' === typeof self && self || 'object' === typeof global && global || {});
